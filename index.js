71c49b16771f889884a9c67384f1defad9eb9236
tijan <tijanmdR@gmail.com>
2024-06-27 22:55:19 +1000

    added dotenv package to access environment variables set up in .env file

--- index.js
+++ index.js
@@ -1,8 +1,21 @@
 const express = require('express');
 const app = express();
 
+// to import .env variables
+require('dotenv').config();
+
+// use this route to make sure the api is working fine
 app.get('/check', (req, res) => {
     return res.json({message: 'working'});
+});
+
+// API route to summarize URL
+// URL should be http://localhost:3000/summarize_url?url=https://example_link.com
+app.get('/summarize_url', async (req, res) => {
+    if (req.query?.url)
+        return res.json(req.query.url);
+    else
+        return res.json({message: 'Query Paramater not found'});
 });
 
 const PORT = process.env.PORT || 3000;
