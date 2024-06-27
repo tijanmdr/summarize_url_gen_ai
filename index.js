b884907c12a105d2f82072e29c9c54c1eca19994
tijan <tijanmdR@gmail.com>
2024-06-27 23:05:24 +1000

    finished summarizing

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const app = express();
+const {GoogleGenerativeAI} = require('@google/generative-ai');
+const {config} = require("dotenv");
 
 // to import .env variables
 require('dotenv').config();
@@ -12,9 +14,17 @@ app.get('/check', (req, res) => {
 // API route to summarize URL
 // URL should be http://localhost:3000/summarize_url?url=https://example_link.com
 app.get('/summarize_url', async (req, res) => {
-    if (req.query?.url)
-        return res.json(req.query.url);
-    else
+    if (req.query?.url) {
+        const prompt = `As a news website, summarize me this link with only 250 lines and one paragraph only: ${req.query.url}`;
+
+
+        const genAI = new GoogleGenerativeAI(process.env.API_KEY);
+        const model = genAI.getGenerativeModel({model: 'gemini-pro'});
+
+        const result = await model.generateContent(prompt);
+        const response = await result.response;
+        return res.json({'result': response.text()});
+    } else
         return res.json({message: 'Query Paramater not found'});
 });
 
