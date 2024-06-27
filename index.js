c5a0695dc43d9fc208b4dd9dc09d840adeb3a350
tijan <tijanmdR@gmail.com>
2024-06-27 22:45:23 +1000

    port setup

--- index.js
+++ index.js
@@ -4,3 +4,9 @@ const app = express();
 app.get('/check', (req, res) => {
     return res.json({message: 'working'});
 });
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log("Server Listening on PORT:", PORT);
+});
